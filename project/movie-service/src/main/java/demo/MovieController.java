67553980171f123ab4ea37e559c815e262df1125
BankyKmitl <kh4z1234@gmail.com>
2017-03-06 23:14:01 +0700

    add CORS to MovieController

--- project/movie-service/src/main/java/demo/MovieController.java
+++ project/movie-service/src/main/java/demo/MovieController.java
@@ -2,12 +2,14 @@ package demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @EnableAutoConfiguration
 public class MovieController {
 
