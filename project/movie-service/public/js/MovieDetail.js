d4eadb084948c5bceda1e7a3224056ec006adefa
NATTHANAN KUNCHOKWANIT <natthanank14@gmail.com>
2017-03-11 16:33:32 +0700

    user ajax to get data from service

--- project/movie-service/public/js/MovieDetail.js
+++ project/movie-service/public/js/MovieDetail.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
+    var movie_id = document.getElementById('movie_id').innerHTML;
+    console.log(movie_id);
     $.ajax({
-        url: "http://localhost:9001/movie?id=5"
-    }).then(function(movie) {
-       $('.id').append(movie.id);
-       $('.name_th').append(movie.name_th);
-       $('.name_en').append(movie.name_en);
+        url: "http://localhost:9001/movie?id=" + movie_id
+    }).then(function(movie, status, jqxhr) {
+       $('#name_th').append(movie.name_th);
+       $('#name_en').append(movie.name_en);
        $('#poster').attr('src',movie.poster);
-       $('.description').append(movie.description);
-       $('.release_date').append(movie.release_date);
-       $('.duration').append(movie.duration);
+       $('#description').append(movie.description);
+       $('#release_date').append("Release date : " + movie.release_date);
+       $('#duration').append("Length : " + movie.duration + " นาที");
        $('#trailer').attr('src',movie.trailer);
        $('#cover').attr('src',movie.cover);
 
