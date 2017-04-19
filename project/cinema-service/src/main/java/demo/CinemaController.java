8fa5e1c4330392c1f3d9a2f800b61f30a7bbaa29
Thanoo Chomyad <nui_pk48@hotmail.com>
2017-04-19 12:11:32 +0700

    add api

--- project/cinema-service/src/main/java/demo/CinemaController.java
+++ project/cinema-service/src/main/java/demo/CinemaController.java
@@ -34,4 +34,74 @@ public class CinemaController {
     public List<Cinema> getAllcinema(){
         return this.cinemaRepository.findAll();
     }
+
+    @RequestMapping("/cinemas/major/bkk")
+    public List<Cinema> getMajorBKK(){
+        return this.cinemaRepository.findMajorBangkok();
+    }
+
+    @RequestMapping("/cinemas/major/cen")
+    public List<Cinema> getMajorCEN(){
+        return this.cinemaRepository.findMajorCenter();
+    }
+
+    @RequestMapping("/cinemas/major/east")
+    public List<Cinema> getMajorEAS(){
+        return this.cinemaRepository.findMajorEast();
+    }
+
+    @RequestMapping("/cinemas/major/north")
+    public List<Cinema> getMajorNOR(){
+        return this.cinemaRepository.findMajorNorth();
+    }
+
+    @RequestMapping("/cinemas/major/northeast")
+    public List<Cinema> getMajorESN(){
+        return this.cinemaRepository.findMajorNorthEast();
+    }
+
+    @RequestMapping("/cinemas/major/south")
+    public List<Cinema> getMajorSOU(){
+        return this.cinemaRepository.findMajorSouth();
+    }
+
+    @RequestMapping("/cinemas/major/west")
+    public List<Cinema> getMajorWES(){
+        return this.cinemaRepository.findMajorWest();
+    }
+
+    @RequestMapping("/cinemas/sf/bkk")
+    public List<Cinema> getSFBKK(){
+        return this.cinemaRepository.findSFBangkok();
+    }
+
+    @RequestMapping("/cinemas/sf/cen")
+    public List<Cinema> getSFCEN(){
+        return this.cinemaRepository.findSFCenter();
+    }
+
+    @RequestMapping("/cinemas/sf/east")
+    public List<Cinema> getSFEAS(){
+        return this.cinemaRepository.findSFEast();
+    }
+
+    @RequestMapping("/cinemas/sf/north")
+    public List<Cinema> getSFNOR(){
+        return this.cinemaRepository.findSFNorth();
+    }
+
+    @RequestMapping("/cinemas/sf/northeast")
+    public List<Cinema> getSFESN(){
+        return this.cinemaRepository.findSFNorthEast();
+    }
+
+    @RequestMapping("/cinemas/sf/south")
+    public List<Cinema> getSFSOU(){
+        return this.cinemaRepository.findSFSouth();
+    }
+
+    @RequestMapping("/cinemas/sf/west")
+    public List<Cinema> getSFWES(){
+        return this.cinemaRepository.findSFWest();
+    }
 }
--- project/cinema-service/src/main/java/demo/CinemaRepository.java
+++ project/cinema-service/src/main/java/demo/CinemaRepository.java
@@ -33,4 +33,102 @@ public class CinemaRepository {
 
         return cinemaList;
     }
+
+    @Transactional(readOnly = true)
+    public List<Cinema> findMajorBangkok() {
+        List<Cinema> cinemaList = this.jdbcTemplate.query("SELECT * FROM Cinema WHERE cinema_group='Major' AND cinema_zone='BKK'", new CinemaRowMapper());
+
+        return cinemaList;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cinema> findMajorCenter() {
+        List<Cinema> cinemaList = this.jdbcTemplate.query("SELECT * FROM Cinema WHERE cinema_group='Major' AND cinema_zone='CEN'", new CinemaRowMapper());
+
+        return cinemaList;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cinema> findMajorEast() {
+        List<Cinema> cinemaList = this.jdbcTemplate.query("SELECT * FROM Cinema WHERE cinema_group='Major' AND cinema_zone='EAS'", new CinemaRowMapper());
+
+        return cinemaList;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cinema> findMajorNorthEast() {
+        List<Cinema> cinemaList = this.jdbcTemplate.query("SELECT * FROM Cinema WHERE cinema_group='Major' AND cinema_zone='ESN'", new CinemaRowMapper());
+
+        return cinemaList;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cinema> findMajorNorth() {
+        List<Cinema> cinemaList = this.jdbcTemplate.query("SELECT * FROM Cinema WHERE cinema_group='Major' AND cinema_zone='NOR'", new CinemaRowMapper());
+
+        return cinemaList;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cinema> findMajorSouth() {
+        List<Cinema> cinemaList = this.jdbcTemplate.query("SELECT * FROM Cinema WHERE cinema_group='Major' AND cinema_zone='SOU'", new CinemaRowMapper());
+
+        return cinemaList;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cinema> findMajorWest() {
+        List<Cinema> cinemaList = this.jdbcTemplate.query("SELECT * FROM Cinema WHERE cinema_group='Major' AND cinema_zone='WES'", new CinemaRowMapper());
+
+        return cinemaList;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cinema> findSFBangkok() {
+        List<Cinema> cinemaList = this.jdbcTemplate.query("SELECT * FROM Cinema WHERE cinema_group='SF' AND cinema_zone='BKK'", new CinemaRowMapper());
+
+        return cinemaList;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cinema> findSFCenter() {
+        List<Cinema> cinemaList = this.jdbcTemplate.query("SELECT * FROM Cinema WHERE cinema_group='SF' AND cinema_zone='CEN'", new CinemaRowMapper());
+
+        return cinemaList;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cinema> findSFEast() {
+        List<Cinema> cinemaList = this.jdbcTemplate.query("SELECT * FROM Cinema WHERE cinema_group='SF' AND cinema_zone='EAS'", new CinemaRowMapper());
+
+        return cinemaList;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cinema> findSFNorthEast() {
+        List<Cinema> cinemaList = this.jdbcTemplate.query("SELECT * FROM Cinema WHERE cinema_group='SF' AND cinema_zone='ESN'", new CinemaRowMapper());
+
+        return cinemaList;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cinema> findSFNorth() {
+        List<Cinema> cinemaList = this.jdbcTemplate.query("SELECT * FROM Cinema WHERE cinema_group='SF' AND cinema_zone='NOR'", new CinemaRowMapper());
+
+        return cinemaList;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cinema> findSFSouth() {
+        List<Cinema> cinemaList = this.jdbcTemplate.query("SELECT * FROM Cinema WHERE cinema_group='SF' AND cinema_zone='SOU'", new CinemaRowMapper());
+
+        return cinemaList;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Cinema> findSFWest() {
+        List<Cinema> cinemaList = this.jdbcTemplate.query("SELECT * FROM Cinema WHERE cinema_group='SF' AND cinema_zone='WES'", new CinemaRowMapper());
+
+        return cinemaList;
+    }
 }
