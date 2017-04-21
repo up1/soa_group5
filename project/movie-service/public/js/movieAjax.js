a21d9c1438728877d82e9c11a34eb4cf3dc1c932
kununya1996 <kununya1996@gmail.com>
2017-04-21 15:48:01 +0700

    create showtime page

--- project/movie-service/public/js/movieAjax.js
+++ project/movie-service/public/js/movieAjax.js
@@ -13,38 +13,6 @@ $(document).ready(function() {
                                                                             <p class="name_th"> ${ data[i].name_th} </p>
                                                                             <p class="release_date"> ${ data[i].release_date} </p>
                                                                         </div>`);
-
-            $('#movie-container').append('<div id="' + data[i].id +'" class="movie-content column"></div>');
-
-            $('#'+data[i].id).append(`
-                                                <div class="ui segment card">
-
-                                                        <a href="/movie_desc?id=${data[i].id}" class="image">
-                                                            <img src=${ data[i].poster }>
-                                                        </a>
-                                                            <p class="name_en"> ${ data[i].name_en } </p>
-                                                            <p class="name_th"> ${ data[i].name_th} </p>
-                                                            <p class="release_date"> ${ data[i].release_date} </p>
-                                                            <button class="ui button"> เช็ครอบหนัง </button>
-                                                </div>`);
         });
-
-        console.log(jqxhr);
-
     });
-
-    $.ajax({
-
-            url: "http://localhost:9002/cinemas"
-
-        }).then(function(data, status, jqxhr) {
-            $.each(data, function(i){
-            $('#cinema-dropdown').append('<div id="c-' + data[i].id +'" class="column cinema-content"></div>');
-
-                        $('#c-'+data[i].id).append(`<div class="ui segment card" id="${'ci-'+data[i].id}" onclick="test_select_cinema('${data[i].id}');">
-                                                        <p class="name_en"> ${ data[i].cinema_name_en}</p>
-                                                     </div>`);
-
-            });
-        });
-});
+   });
---
+++ project/movie-service/public/js/showtime.js
@@ -0,0 +1,102 @@
+$(document).ready(function() {
+    var movie_id = document.getElementById('movie_id').innerHTML;
+    var cinema_id = document.getElementById('cinema_id').innerHTML;
+
+    var major_list = [];
+    var sf_list = [];
+
+    console.log(movie_id);
+    console.log(cinema_id);
+
+    if(cinema_id == '[]') {
+        $.ajax({
+
+                url: "http://localhost:9003/showfrommovie?movie=" + movie_id
+
+            }).then(function(showtime, status, jqxhr) {
+                for(var i = 0; i < showtime.length; i++) {
+                    if(showtime[i].cinema.group == 'SF') {
+                        var cinema_id = showtime[i].cinema_id;
+                        if(sf_list.indexOf(cinema_id) == -1) {
+                            sf_list.push(cinema_id);
+                            $('#showtime').append(`<div id="${showtime[i].cinema.id}" class="ui divided items" style="padding:14px;"><h3 class="ui block header left floated">
+                                                                   ${showtime[i].cinema.cinema_name_en}
+                                                               </h3>
+                                                               </div>`);
+                            if(i == 0 || showtime[i].movie_id != showtime[i-1].movie_id || showtime[i].show_theatre != showtime[i-1].show_theatre) {
+                                $('#'+showtime[i].cinema.id).append(`<div class="item">
+                                                                        <div class="head">
+                                                                            <div style="margin: 30px; padding-top: 50px;">
+                                                                                <h2>theater</h2>
+                                                                                <h2 id="theatre">${showtime[i].show_theatre}</h2>
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="image" style="width: 150px">
+                                                                            <img src=${showtime[i].movie.poster}/>
+                                                                        </div>
+                                                                        <div class="content">
+                                                                            <a class="header" href="/movie_desc?id=${showtime[i].movie.id}">${showtime[i].movie.name_en}</a>
+                                                                            <div class="meta">
+                                                                                <span class="cinema">${showtime[i].movie.name_th}</span>
+                                                                                <span class="cinema">${showtime[i].cinema.cinema_name_en}</span>
+                                                                            </div>
+                                                                            <div class="extra">
+                                                                                <div class="ui label">${showtime[i].movie.duration}</div>
+                                                                                <div class="ui label">${showtime[i].show_system_type}</div>
+                                                                                <div class="ui label">ทั่วไป</div>
+                                                                                <div class="ui label"><i class="globe icon"></i>${showtime[i].show_language}</div>
+                                                                            </div>
+                                                                            <div id="${showtime[i].show_theatre}" class="description">
+                                                                                <button class="ui red basic button">${showtime[i].show_time}</button>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>`);
+                            } else {
+                                console.log("showtime = " + showtime[i].showtime_id);
+                                $("#"+showtime[i].show_theatre).append(`<button class="ui red basic button">${showtime[i].show_time}</button>`);
+                            }
+
+                        } else {
+                            if(i == 0 || showtime[i].movie_id != showtime[i-1].movie_id || showtime[i].show_theatre != showtime[i-1].show_theatre) {
+                                $('#'+showtime[i].cinema.id).append(`<div class="item">
+                                                                        <div class="head">
+                                                                            <div style="margin: 30px; padding-top: 50px;">
+                                                                                <h2>theater</h2>
+                                                                                <h2 id="theatre">${showtime[i].show_theatre}</h2>
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="image" style="width: 150px">
+                                                                            <img src=${showtime[i].movie.poster}/>
+                                                                        </div>
+                                                                        <div class="content">
+                                                                            <a class="header" href="/movie_desc?id=${showtime[i].movie.id}">${showtime[i].movie.name_en}</a>
+                                                                            <div class="meta">
+                                                                                <span class="cinema">${showtime[i].movie.name_th}</span>
+                                                                                <span class="cinema">${showtime[i].cinema.cinema_name_en}</span>
+                                                                            </div>
+                                                                            <div class="extra">
+                                                                                <div class="ui label">${showtime[i].movie.duration}</div>
+                                                                                <div class="ui label">${showtime[i].show_system_type}</div>
+                                                                                <div class="ui label">ทั่วไป</div>
+                                                                                <div class="ui label"><i class="globe icon"></i>${showtime[i].show_language}</div>
+                                                                            </div>
+                                                                            <div id="${showtime[i].show_theatre}" class="description">
+                                                                                <button class="ui red basic button">${showtime[i].show_time}</button>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>`);
+                                             } else {
+                                                 console.log("showtime = " + showtime[i].showtime_id);
+                                                 $('#'+showtime[i].show_theatre+ ' .description').append(`<button class="ui red basic button">${showtime[i].show_time}</button>`);
+                                             }
+                        }
+                    }
+                }
+
+                console.log(jqxhr);
+            });
+    }
+
+
+
+});
