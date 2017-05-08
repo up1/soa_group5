656c3ebc766358be01f20569673e58593884879f
kununya1996 <kununya1996@gmail.com>
2017-05-08 23:03:50 +0700

    Update ResultMessage

--- project/user_service/src/main/java/demo/UserController.java
+++ project/user_service/src/main/java/demo/UserController.java
@@ -4,10 +4,13 @@ package demo; /**
 import demo.adapter.Movie;
 import demo.exceptions.InvalidTokenException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
+
 
 @CrossOrigin("*")
 @RestController
@@ -33,19 +36,21 @@ public class UserController {
 
     @PostMapping(value = "/register")
     @ResponseBody
-    public void registerUser(@RequestParam(value="username") String username,
-                               @RequestParam(value="password") String password,
-                               @RequestParam(value="firstname") String firstname,
-                               @RequestParam(value="lastname") String lastname,
-                               @RequestParam(value="gender") String gender,
-                               @RequestParam(value="birth_date") String birth_date,
-                               @RequestParam(value="email") String email,
-                               @RequestParam(value="noti_status") String noti_status) throws IOException {
+    public ResponseEntity registerUser(@RequestParam(value="username") String username,
+                                       @RequestParam(value="password") String password,
+                                       @RequestParam(value="firstname") String firstname,
+                                       @RequestParam(value="lastname") String lastname,
+                                       @RequestParam(value="gender") String gender,
+                                       @RequestParam(value="birth_date") String birth_date,
+                                       @RequestParam(value="email") String email,
+                                       @RequestParam(value="noti_status") String noti_status) throws IOException {
         this.userRepository.userRegister(username, password, firstname, lastname, gender, birth_date, email, noti_status);
+        ResultMessage ResultMessage = new ResultMessage(username, "Successfully Created");
+        return new ResponseEntity(ResultMessage , HttpStatus.CREATED);
     }
 
     @PutMapping("/edit")
-    public void editUserDetails(@RequestParam(value="username") String username,
+    public ResponseEntity editUserDetails(@RequestParam(value="username") String username,
                                  @RequestParam(value="firstname") String firstname,
                                  @RequestParam(value="lastname") String lastname,
                                  @RequestParam(value="gender") String gender,
@@ -53,13 +58,17 @@ public class UserController {
                                  @RequestParam(value="email") String email,
                                  @RequestParam(value="noti_status") String noti_status) {
         this.userRepository.userEditDetails(username, firstname, lastname, gender, birth_date, email, noti_status);
+        ResultMessage ResultMessage = new ResultMessage(username, "Successfully Update");
+        return new ResponseEntity(ResultMessage , HttpStatus.CREATED);
     }
 
     @PutMapping("/changepassword")
-    public void userChangePassword(@RequestParam(value="username") String username,
-                                     @RequestParam(value="password") String password,
-                                     @RequestParam(value="new_password") String new_password){
+    public ResponseEntity userChangePassword(@RequestParam(value="username") String username,
+                                             @RequestParam(value="password") String password,
+                                             @RequestParam(value="new_password") String new_password){
         this.userRepository.userChangePassword(username, password, new_password);
+        ResultMessage ResultMessage = new ResultMessage(username, "Password Changed");
+        return new ResponseEntity(ResultMessage , HttpStatus.CREATED);
     }
 
     @GetMapping("/{username}/likes")
@@ -84,8 +93,10 @@ public class UserController {
     }
 
     @PostMapping("/logout")
-    public void logout(@RequestParam String tokenID){
+    public ResponseEntity logout(@RequestParam String tokenID){
         this.userRepository.logout(tokenID);
+        ResultMessage ResultMessage = new ResultMessage("username","User Logout");
+        return new ResponseEntity(ResultMessage , HttpStatus.CREATED);
     }
 
     @GetMapping("/validate/{tokenID}")
